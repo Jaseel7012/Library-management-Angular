1ec7d57b43e936f45d6c2753e56e72d1757daaef
jaseel <jaseeljctcs@gmail.com>
2022-11-24 19:56:56 +0530

    2nd commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LibraryAdminLogComponent } from './library-admin-log/library-admin-log.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LibraryAdminLogComponent
   ],
   imports: [
     BrowserModule,

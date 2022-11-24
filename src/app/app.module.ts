829a15a486acf4b29fb9b738e103dc5574df81d2
jaseel <jaseeljctcs@gmail.com>
2022-11-24 20:24:18 +0530

    adm reg branch initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LibraryAdminLogComponent } from './library-admin-log/library-admin-log.component';
+import { LibraryAdminRegComponent } from './library-admin-reg/library-admin-reg.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LibraryAdminLogComponent
+    LibraryAdminLogComponent,
+    LibraryAdminRegComponent
   ],
   imports: [
     BrowserModule,
